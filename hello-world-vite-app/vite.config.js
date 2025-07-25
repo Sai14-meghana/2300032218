9fe9a1e8b72fe58ee61293819fab6ed9102b854d
Sai14-meghana <saimeghanakonduru@gmail.com>
2025-07-25 10:58:39 +0530

    Initial commit

--- hello-world-vite-app/vite.config.js
+++ hello-world-vite-app/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2300032218/',  // ✅ Add this line
+  base: '/2300032218/',  
 })
